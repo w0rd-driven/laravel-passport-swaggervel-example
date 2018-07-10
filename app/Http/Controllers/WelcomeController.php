45e6f4eaf31186f0a40c89cecc40330bc3167f1c
Jeremy <jeremy@braytonium.com>
2018-07-09 21:06:12 -0400

    Converted route closure to a proper WelcomeController to facilitate proper `artisan route:cache` usage in the future.

---
+++ app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WelcomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        return view('welcome');
+    }
+}
