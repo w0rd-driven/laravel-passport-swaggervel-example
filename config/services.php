ff9cd2e66a24671b75bd036efdacc5bf101592d3
Jeremy <jeremy@braytonium.com>
2018-07-09 22:13:20 -0400

    Personal preference: moved generic App\User to App\Models\Api\v1 namespace instead to show we're versioning all the things.

--- config/services.php
+++ config/services.php
@@ -30,7 +30,7 @@ return [
     ],
 
     'stripe' => [
-        'model' => App\User::class,
+        'model' => App\Models\Api\v1\User::class,
         'key' => env('STRIPE_KEY'),
         'secret' => env('STRIPE_SECRET'),
     ],
